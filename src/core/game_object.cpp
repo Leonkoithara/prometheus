4888908a962ed7d8dd8d78dd9002644ba1e8245e
Leon Joe K <leon.koithara@gmail.com>
2023-01-31 00:36:36 +0530

    Add button text

--- src/core/game_object.cpp
+++ src/core/game_object.cpp
@@ -1,13 +1,12 @@
 #include "game_object.h"
-#include <SDL_image.h>
-#include <SDL_render.h>
+#include "SDL_render.h"
+
 #include <iostream>
 
 GameObject::GameObject(std::string nm)
 {
 	name = nm;
 	std::cout << "Game object created" << std::endl;
-	texture = NULL;
 	position = {0, 0, 0};
 	src_rect = {0, 0, 0, 0};
 	dest_rect = {0, 0, 0, 0};
@@ -16,12 +15,18 @@ GameObject::GameObject(std::string nm)
 
 GameObject::~GameObject() {}
 
-void GameObject::set_texturefile(SDL_Renderer *renderer, std::string filename)
+void GameObject::set_render_rect_defaults()
 {
-	SDL_Surface *surface = IMG_Load(filename.c_str());
-	texture = SDL_CreateTextureFromSurface(renderer, surface);
-	SDL_FreeSurface(surface);
-	SDL_QueryTexture(texture, NULL, NULL, &src_rect.w, &src_rect.h);
+	int w1 = 0, w2 = 0, h1 = 0, h2 = 0;
+	for (auto it : textures) {
+	    SDL_QueryTexture(it.second.second, NULL, NULL, &w2, &h2);
+		if(w2 > w1)
+			w1 = w2;
+		if(h2 > h1)
+			h1 = h2;
+	}
+	src_rect.h = h1;
+	src_rect.w = w1;
 	dest_rect.h = src_rect.h;
 	dest_rect.w = src_rect.w;
 }
