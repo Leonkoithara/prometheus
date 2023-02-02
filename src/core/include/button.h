e1dda0def9722df30cfb5562635d2d584d34d7b8
Leon Joe K <leon.koithara@gmail.com>
2023-02-03 00:27:45 +0530

    Button callback from class methods to func ptrs

--- src/core/include/button.h
+++ src/core/include/button.h
@@ -10,19 +10,23 @@ class Button : public GameObject
 private:
 	int clicked;
 	std::string text;
+
+	void (*whileclickevent)(int);
+	void (*onceclickevent)(int);
 public:
 	Button(std::string);
 	~Button();
 
 	bool check_clicked(int, int);
 	void click_object(int, bool);
-	void whileclickevent(int);
-	void onceclickevent(int);
 	void update();
 
 	void set_text(std::string);
 
 	std::string get_text() { return text; }
+
+	void set_whileclickevent(void (*func)(int)) { whileclickevent = func;}
+	void set_onclickevent(void (*func)(int)) { onceclickevent = func; }
 };
 
 
