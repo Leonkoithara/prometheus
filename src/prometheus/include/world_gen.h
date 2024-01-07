713068f5179ba4fa06248118108c2e5abfb0dc7a
Leonkoithara <36569141+Leonkoithara@users.noreply.github.com>
2024-01-08 00:16:15 +0530

    Scroll camera with wasd

--- src/prometheus/include/world_gen.h
+++ src/prometheus/include/world_gen.h
@@ -8,8 +8,10 @@
 #define TERRAIN_TYPES 6
 
 enum terrain { plains = 0, mountains, forests, water, human_civ, civ_ruins };
+enum direction { up = 0, down, right, left };
 
 
+direction convert2direction(std::string dir);
 void start_create_world(int, GameObject *);
 void create_new_world(Scene*, std::string, int = 50, long = 0);
 #endif
