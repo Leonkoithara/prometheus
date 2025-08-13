1e05e3be8661dd2edf8c06a0378fda44e0cecf63
Leon Joe K <36569141+Leonkoithara@users.noreply.github.com>
2025-08-13 23:08:43 +0530

    Add ground

--- src/prometheus/start_world_gen.cpp
+++ src/prometheus/start_world_gen.cpp
@@ -5,7 +5,6 @@
 #include <ctime>
 
 #include <button.h>
-#include <camera.h>
 #include <scene.h>
 #include <game_manager.h>
 #include <game_object.h>
@@ -14,28 +13,6 @@
 
 
 WorldGen *world;
-
-void camera_translate(int x, GameObject *obj)
-{
-    enum direction { up = 0, down, right, left };
-    int dir = obj->get_tag("direction")->longval;
-
-    switch (dir)
-	{
-        case up:
-            cam.camera_translate({0, -10});
-            break;
-        case down:
-            cam.camera_translate({0, 10});
-            break;
-        case right:
-            cam.camera_translate({10, 0});
-            break;
-        case left:
-            cam.camera_translate({-10, 0});
-            break;
-    }
-}
 
 void create_new_world(Scene *prometheus, std::string world_name, int world_size)
 {
@@ -59,24 +36,7 @@ void start_create_world(int x, GameObject *)
     if (winy > screen_size.y)
         winy = screen_size.y;
 
-    Button *scroll_up = new Button("worldmap_w", 'w');
-    scroll_up->add_tag("direction", (long)0);
-    Button *scroll_down = new Button("worldmap_s", 's');
-    scroll_down->add_tag("direction", 1);
-    Button *scroll_right = new Button("worldmap_d", 'd');
-    scroll_right->add_tag("direction", 2);
-    Button *scroll_left = new Button("worldmap_a", 'a');
-    scroll_left->add_tag("direction", 3);
-    scroll_up->set_onclickevent(&camera_translate);
-    scroll_down->set_onclickevent(&camera_translate);
-    scroll_right->set_onclickevent(&camera_translate);
-    scroll_left->set_onclickevent(&camera_translate);
-
     new_world_gen->create_window("Prometheus", 0, 0, winx, winy, {0, 0, 0}, false);
-    new_world_gen->add_game_object(scroll_up);
-    new_world_gen->add_game_object(scroll_down);
-    new_world_gen->add_game_object(scroll_right);
-    new_world_gen->add_game_object(scroll_left);
 
     create_new_world(new_world_gen, world_name, world_size);
 
